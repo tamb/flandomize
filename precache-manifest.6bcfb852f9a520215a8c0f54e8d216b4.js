28da098a5bc2c3424b721e87b9b3e4b045f11b51
Tom Saporito <tamb89@protonmail.com>
2020-04-13 10:49:11 -0400

    Updates

---
+++ precache-manifest.6bcfb852f9a520215a8c0f54e8d216b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eed667e677c298f805725be1521ae51",
+    "url": "/flandomize/index.html"
+  },
+  {
+    "revision": "4a3ac9db551a7fe15f18",
+    "url": "/flandomize/static/css/2.9d71b1d6.chunk.css"
+  },
+  {
+    "revision": "12dca529429b3e626de0",
+    "url": "/flandomize/static/css/main.0d10d777.chunk.css"
+  },
+  {
+    "revision": "4a3ac9db551a7fe15f18",
+    "url": "/flandomize/static/js/2.428385e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/flandomize/static/js/2.428385e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12dca529429b3e626de0",
+    "url": "/flandomize/static/js/main.677d1605.chunk.js"
+  },
+  {
+    "revision": "5dd29f199d5e25329bdd",
+    "url": "/flandomize/static/js/runtime-main.fd16c62e.js"
+  },
+  {
+    "revision": "914d6585458f655004cd15d1266e56cb",
+    "url": "/flandomize/static/media/mustache-clipart-7.914d6585.png"
+  }
+]);
